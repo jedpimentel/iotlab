4d8b3cf0b7ef50622775f8b62d25106b597b8470
jedpimentel <jedpimentel@gmail.com>
2021-01-15 21:42:42 -0500

    connects to local wifi

---
+++ mkrwifi1010/marionette/marionette.ino
@@ -0,0 +1,88 @@
+#include <WiFiNINA.h>
+#include "arduino_secrets.h"
+
+const char separator[] = "----------------"; 
+const char ssid[] = SECRET_SSID;//network name string
+const char pass[] = SECRET_PASS;//network password string
+int status = WL_IDLE_STATUS;//WiFi radio status
+
+/* I'm starting off referencing the MKRWiFi1010's 
+ * "connectingto a wifi network" tutorial as a starting
+ * point
+  */
+void setup() {
+  // initialize serial and wait for port to opwn
+  Serial.begin(9600);
+  while (!Serial);
+
+  listNetworks();
+
+  //attempt to connect to wifi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to network:");
+    Serial.println(ssid);
+    Serial.println(pass);
+    //connect to wpa/wpa2 network:
+    status = WiFi.begin(ssid, pass);
+
+    //wait 10 seconds for connection
+    //seems excesive to me but okay
+    delay(10000);
+  }
+
+  Serial.println("Device is connected to network");
+  Serial.println(separator);
+  printData();
+  Serial.println(separator);
+}
+
+void loop() {
+  //check the network connection once every 10 seonds:
+  delay(10000);
+  printData();
+  Serial.println(separator);
+}
+
+void printData() {
+  Serial.println("Board Information:");
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  Serial.println();
+  Serial.println("Network Information:");
+  Serial.print("SSID:");
+  Serial.println(WiFi.SSID());
+
+  //print the received signal strength
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.println(rssi);
+}
+
+// https://www.arduino.cc/en/Reference/WiFiNINAScanNetworks
+void listNetworks() {
+  // scan for nearby networks:
+  Serial.println("** Scan Networks **");
+  int numSsid = WiFi.scanNetworks();
+  if (numSsid == -1) {
+    Serial.println("Couldn't get a WiFi connection");
+    while (true);
+  }
+
+  // print the list of networks seen:
+  Serial.print("number of available networks:");
+  Serial.println(numSsid);
+
+  // print the network number and name for each network found:
+  for (int thisNet = 0; thisNet < numSsid; thisNet++) {
+    Serial.print(thisNet);
+    Serial.print(") ");
+    Serial.print(WiFi.SSID(thisNet));
+    Serial.print("\tSignal: ");
+    Serial.print(WiFi.RSSI(thisNet));
+    Serial.println(" dBm");
+//    Serial.print("\tEncryption: ");
+//    printEncryptionType(WiFi.encryptionType(thisNet));
+  }
+}
---
+++ mkrwifi1010/readme.txt
@@ -0,0 +1,9 @@
+marionette.ino requires an arduino_secrets.h header file
+
+- create a "arduino_secrets.h" file inside "marionette" folder,
+reference: https://www.arduino.cc/en/Guide/MKRWiFi1010/connecting-to-wifi-network
+
+marionette/arduino_secrets.h should contain:
+
+
+
