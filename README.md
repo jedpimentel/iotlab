347871d07e14542f09333f2f571c5d15a4020163
jedpimentel <jedpimentel@gmail.com>
2021-01-18 16:54:36 -0500

    implement timezone logic

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # iotlab
 
 mkrwifi1010/marionette is my first attempt at using the arduino mkrwifi1010.
+
+Currently, Marionette can connect to a local wifi, and gets a network time.
+
+Roadmap:
+	- Handle situations where network isn't available on startup
+	- Wake/Sleep cycle
+
+Bug Note:
+	- there's no daylight savings logic 
--- mkrwifi1010/marionette/marionette.ino
+++ mkrwifi1010/marionette/marionette.ino
@@ -2,7 +2,7 @@
 #include <WiFiNINA.h>
 #include <RTCZero.h>
 
-IPAddress ip(SECRET_IP[0], SECRET_IP[1], SECRET_IP[2], SECRET_IP[3]);
+//IPAddress ip(SECRET_IP[0], SECRET_IP[1], SECRET_IP[2], SECRET_IP[3]);
 
 const char separator[] = "----------------"; 
 const char ssid[] = SECRET_SSID;//network name string
@@ -12,11 +12,8 @@ WiFiServer server(80);
 WiFiClient client = server.available();
 
 RTCZero rtc;
+const int timeZone = -5;//TODO: move into a config file
 
-/* I'm starting off referencing the MKRWiFi1010's 
- * "connectingto a wifi network" tutorial as a starting
- * point
-  */
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
@@ -24,21 +21,23 @@ void setup() {
   Serial.begin(9600);
   while (!Serial);
 
-  listNetworks();
+//  listNetworks();
 
-  //attempt to connect to wifi network:
-//  setting custom IP was breaking the real time clock functionality
+//  attempt to connect to wifi network:
 //  WiFi.config(ip);
   while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to network:");
+    Serial.print("Attempting to connect to network: ");
     Serial.println(ssid);
-    Serial.println(pass);
-    //connect to wpa/wpa2 network:
+//    Serial.println(pass);
+//    connect to wpa/wpa2 network:
     status = WiFi.begin(ssid, pass);
 
-    //wait 10 seconds for connection
-    //seems excesive to me but okay
-    delay(10000);
+//    wait 10-ish seconds for connection
+//    seems excesive to me but okay
+//    delay(10000);
+    for (int mSec = 10000; status != WL_CONNECTED && mSec > 0; mSec--) {
+      delay(1);
+    }
   }
 
   Serial.println("Device is connected to network");
@@ -55,15 +54,18 @@ void setup() {
     
     unsigned long epoch;
     int attempts = 0, maxAttempts = 6;
+    Serial.print("Getting Network Time");
     do {
       epoch = WiFi.getTime();
-      Serial.println(epoch);
+      Serial.print(".");
       attempts++;
       if (epoch == 0) {
         delay(2000);
       }
     }
     while (epoch == 0 && attempts < maxAttempts);
+    Serial.println();
+    
     if (attempts == maxAttempts) {
       Serial.println("Network Time Protocol unreachable :(");
       Serial.println("reset device to try again");
@@ -73,17 +75,21 @@ void setup() {
     else {
       Serial.println("Epoch received:");
       Serial.println(epoch);
-      rtc.setEpoch(epoch);
+      rtc.setEpoch(epoch + timeZone * 60 * 60);
       Serial.println();
     }
+
+    printDateTime();
+    Serial.println();
   }
+
+  Serial.println("Initialization Good");
 }
 
 void loop() {
-  //check the network connection once every 10 seonds:
   delay(10000);
-  printData();
-  Serial.println(separator);
+//  printData();
+//  Serial.println(separator);
 }
 
 void printData() {
@@ -128,4 +134,38 @@ void listNetworks() {
 //    Serial.print("\tEncryption: ");
 //    printEncryptionType(WiFi.encryptionType(thisNet));
   }
+}
+
+//https://en.wikipedia.org/wiki/ISO_8601
+void printDateTime() {
+  printDate();
+  Serial.print("L");
+  printTime();
+}
+void printTime() {
+//  the "print2digits(rtc.getHours() + GMT);" line in reference feels 
+// like it might break near beggining or end of day
+// I'm going against reference, and instead implementing the time offset clockside
+//https://www.arduino.cc/en/Tutorial/WiFiRTC
+//  print2digits(rtc.getHours() + timeZone);
+  print2digits(rtc.getHours());
+  Serial.print(":");
+  print2digits(rtc.getMinutes());
+  Serial.print(":");
+  print2digits(rtc.getSeconds());
+}
+
+void printDate() {
+  Serial.print(rtc.getYear());
+  Serial.print("-");
+  print2digits(rtc.getMonth());
+  Serial.print("-");
+  print2digits(rtc.getDay());
+}
+
+void print2digits(int number) {
+  if (number < 10) {
+    Serial.print("0");
+  }
+  Serial.print(number);
 }
